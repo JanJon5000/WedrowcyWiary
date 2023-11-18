08677b6e72a5515626d0057a73e5a5be0ae4b203
JanJon5000 <jan@ipoczta.pl>
2023-11-18 10:03:15 +0100

    basic post model created - more development in the future

--- kosciol/index/models.py
+++ kosciol/index/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField(max_length=250)
+    slug = models.SlugField()
+
+    published = models.DateField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=ordering)
+        ]
+
+    def __str__(self):
+        return self.title
+    
+    def publish(self):
+        self.save()
