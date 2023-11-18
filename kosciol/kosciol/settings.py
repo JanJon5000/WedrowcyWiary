a9040d8f79048ea0dd078d0ef02102043eadc0a5
JanJon5000 <jan@ipoczta.pl>
2023-11-18 09:26:14 +0100

    index app

--- kosciol/kosciol/settings.py
+++ kosciol/kosciol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p6lo=26k3nga(^y*7hbrppmy^wv2$%-%$o(iiyejc!q7z#i08s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonwanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
 TIME_ZONE = 'UTC'
 
