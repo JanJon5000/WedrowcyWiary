0fd511834461f1a07993a440ad9bd48f0de5b5c6
JanJon5000 <jan@ipoczta.pl>
2023-12-10 22:10:38 +0100

    bug fixes

--- kosciol/kosciol/settings.py
+++ kosciol/kosciol/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'index\\static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'index/static')
 
 # STATICFILES_FINDERS = ['compressor.finders.CompressorFinder']
 
