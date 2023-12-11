dacdd18f6c3b4bc3b292735d6e7f4ed4a86eaac1
JanJon5000 <WedrowcyWiary@4146c987a9a6>
2023-12-11 20:51:24 +0000

    _

--- kosciol/kosciol/settings.py
+++ kosciol/kosciol/settings.py
@@ -118,13 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'index/static')
+
+STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR / 'media')
